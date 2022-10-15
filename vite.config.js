db82106ee21f1701ea9e4ed3fd4a497bdd3828c9
Marvin Ezequiel Gonzáles Hernández <mg082337@gmail.com>
2022-10-15 13:31:54 -0600

    funtional2

--- vite.config.js
+++ vite.config.js
@@ -3,6 +3,6 @@ import react from '@vitejs/plugin-react'
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  base: 'https://soymegh.github.io/',
+  base: 'https://soymegh.github.io/gh-react/',
   plugins: [react()]
 })
